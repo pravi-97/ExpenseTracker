8146934170745f2e9a26b56126b938057cb6dbdd
pravi-97 <129660831+pravi-97@users.noreply.github.com>
2024-03-17 11:03:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const { createClient } = require("@libsql/client");  // Use require for Node.js
+const { createClient } = require("@libsql/client");
 const express = require('express')
 const app = express()
 const port = 3000
 
 async function fetchData() {
-    const client = createClient({  // Create client outside the route handler
+    const client = createClient({  
         url: "libsql://test-db-pravi-97.turso.io",
         authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3MTExNTIwMDYsImlhdCI6MTcxMDU3OTUwNCwiaWQiOiJjOWQ3ZTRlNS1jMzM2LTExZWUtYjA3MS01MjA1M2M0YjIzYTYifQ.4kWZrwu1ChjtJkzNaKens6FJjF1MamUxspzHs4u27SMb1gxcQrTgn7YupNysw4imHv3GDDDq5hfQYyCNe-FYAA",
     });
     try {
-        const result = await client.execute("SELECT * FROM users");
-        return result;
+        const result = await client.execute("SELECT * FROM expense");
+        return result.rows;
     } finally {
-        // Consider closing the client connection here for proper resource management
-        await client.close();  // Optional, but recommended for long-running applications
+        await client.close();
     }
 }
 
@@ -23,7 +22,16 @@ app.get('/', async (req, res) => {
         res.send(response);
     } catch (error) {
         console.error('Error :', error);
-        res.status(500).send('Error fetching data');  // Handle error gracefully with a response
+        res.status(500).send('Error fetching data');  
+    }
+})
+
+app.post('/', async (req, res) =>{
+    try{
+
+    }
+    catch(error){
+
     }
 })
 
