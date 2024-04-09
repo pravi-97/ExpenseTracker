00aa38266ef1ffd66c37a0e58b5f7d364b695db3
pravi-97 <praveesh.894@gmail.com>
2024-04-09 23:44:04 +0530

    update get monthly

--- index.js
+++ index.js
@@ -123,20 +123,18 @@ app.get('/monthly', async (req, res) => {
     try {
         if (req.query.month.trim() == "" || req.query.month == undefined || req.query.year.trim() == "" || req.query.year == undefined){
             // const response = await pool.query(`SELECT type, CAST(sum(price) AS numeric) AS total_price FROM expenses where deleted = false group by type`);
-            const response = await client.execute(`SELECT type, CAST(SUM(price) AS NUMERIC) AS total_price FROM expenses WHERE deleted = 0 GROUP BY type;`);
+            const response = await client.execute(`SELECT type, SUM(price) AS total_price FROM expenses WHERE deleted = 0 GROUP BY type;`);
             // console.log(result.rows);
             // const formattedData = response.rows.map(row => {
             //     row.total_price = parseFloat(row.total_price);
             //     return row;
             // });
-            const month = req.query.month.padStart(2, '0');
-            const year = req.query.year;
             res.status(200).send(response.rows);
         }else{
             const month = req.query.month.padStart(2, '0'); 
             const year = req.query.year;
             // const response = await pool.query(`SELECT type, CAST(sum(price) AS numeric) AS total_price FROM expenses WHERE EXTRACT(MONTH FROM date) = ${req.query.month} AND EXTRACT(YEAR FROM date) = ${req.query.year}  AND deleted = false group by type`);
-            const response = await client.execute(`SELECT type, SUM(price) AS total_price FROM expenses WHERE strftime('%m', date) = '${month}' AND strftime('%Y', date) = '${year}' AND deleted = false GROUP BY type;`);
+            const response = await client.execute(`SELECT type, SUM(price) AS total_price FROM expenses WHERE strftime('%m', date) = '${month}' AND strftime('%Y', date) = '${year}' AND deleted = 0 GROUP BY type;`);
             const formattedData = response.rows.map(row => {
                 row.total_price = parseFloat(row.total_price);
                 return row;
