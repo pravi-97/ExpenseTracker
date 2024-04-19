4221cc201cd3554ac7e4e3d9e7fb4783a3d533fd
pravi-97 <praveesh.894@gmail.com>
2024-04-19 13:06:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/all', async (req, res) => {
     try {
         const response = await client.execute(`SELECT * FROM expenses WHERE deleted = 0 and userid = '${req.body.userid}' ORDER BY date DESC;`);
         console.log("query: ", `SELECT * FROM expenses WHERE deleted = 0 and userid = '${req.body.userid}' ORDER BY date DESC;`);
+        console.log(req.body);
         res.status(200).send(response.rows);
     } catch (error) {
         console.error('Error :', error);
